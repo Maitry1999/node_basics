068d86d6df55ec731fac2a20d374d42375955905
maitry <maitry.netsol@gmail.com>
2024-11-29 14:01:31 +0530

    @maitry : remove unused files

--- db/users.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose'); const productSchema = new mongoose.Schema({
-
-    email: String,
-    password: String,
-});
-const product = mongoose.model('users', productSchema);
-
-module.exports = product;
